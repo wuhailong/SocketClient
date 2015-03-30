5a4a4b78c46d7c1a96d78ed12a4fc58c6f6f7195
huzuohuyou <1255246777@qq.com>
2015-03-30 17:51:19 +0800

    1.0.0.1
    socket客户端

---
+++ SocketClient/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace SocketClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txt_message = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_ip = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_port = new System.Windows.Forms.TextBox();
+            this.rch_back = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(193, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "连接服务器";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(193, 34);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "断开连接";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(191, 285);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "发送消息";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // txt_message
+            // 
+            this.txt_message.Location = new System.Drawing.Point(12, 285);
+            this.txt_message.Name = "txt_message";
+            this.txt_message.Size = new System.Drawing.Size(160, 21);
+            this.txt_message.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ip";
+            // 
+            // txt_ip
+            // 
+            this.txt_ip.Location = new System.Drawing.Point(72, 7);
+            this.txt_ip.Name = "txt_ip";
+            this.txt_ip.Size = new System.Drawing.Size(102, 21);
+            this.txt_ip.TabIndex = 4;
+            this.txt_ip.Text = "127.0.0.1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Port";
+            // 
+            // txt_port
+            // 
+            this.txt_port.Location = new System.Drawing.Point(72, 36);
+            this.txt_port.Name = "txt_port";
+            this.txt_port.Size = new System.Drawing.Size(102, 21);
+            this.txt_port.TabIndex = 4;
+            this.txt_port.Text = "3000";
+            // 
+            // rch_back
+            // 
+            this.rch_back.Location = new System.Drawing.Point(12, 86);
+            this.rch_back.Name = "rch_back";
+            this.rch_back.Size = new System.Drawing.Size(256, 193);
+            this.rch_back.TabIndex = 5;
+            this.rch_back.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "返回信息";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(280, 318);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.rch_back);
+            this.Controls.Add(this.txt_port);
+            this.Controls.Add(this.txt_ip);
+            this.Controls.Add(this.txt_message);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Clicent";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox txt_message;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_ip;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_port;
+        private System.Windows.Forms.RichTextBox rch_back;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ SocketClient/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace SocketClient
+{
+    public partial class Form1 : Form
+    {
+        Socket cc = null;
+        Thread clientThread = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+             
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+             try
+            {
+                int port = int.Parse(txt_port.Text);
+                string host = txt_ip.Text;
+                //创建终结点EndPoint
+                IPAddress ip = IPAddress.Parse(host);
+                IPEndPoint ipe = new IPEndPoint(ip, port);   //把ip和端口转化为IPEndPoint的实例
+                //创建Socket并连接到服务器
+                Socket c = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);   //  创建Socket
+                cc = c;
+                c.Connect(ipe); //连接到服务器
+                clientThread = new Thread(new ThreadStart(ReceiveData));
+                clientThread.Start();
+                SetText("Connecting...");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("argumentNullException:{0}", ex);
+            }
+            catch (SocketException exp)
+            {
+                Console.WriteLine("SocketException:{0}",exp);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //向服务器发送信息
+            string sendStr = txt_message.Text;
+            byte[] bs = Encoding.ASCII.GetBytes(sendStr);   //把字符串编码为字节
+            cc.Send(bs, bs.Length, 0); //发送信息
+            rch_back.Text += "\n 我：" + sendStr;
+            txt_message.Text = "";
+        }
+
+        public delegate void SetTextHandler(string text);
+        private void SetText(string text)
+        {
+            if (rch_back.InvokeRequired == true)
+            {
+                SetTextHandler set = new SetTextHandler(SetText);//委托的方法参数应和SetText一致 
+                rch_back.Invoke(set, new object[] { text }); //此方法第二参数用于传入方法,代替形参text 
+            }
+            else
+            {
+                rch_back.Text += text;
+            }
+        } 
+
+        public void ReceiveData()
+        {
+            try
+            {
+                while (true)
+                {
+                    //接受从服务器返回的信息
+                    string recvStr = "";
+                    byte[] recvBytes = new byte[1024];
+                    int bytes;
+                    bytes = cc.Receive(recvBytes, recvBytes.Length, 0);    //从服务器端接受返回信息
+                    recvStr = "\n Server:" + Encoding.ASCII.GetString(recvBytes, 0, bytes);
+                    SetText(recvStr);
+                }
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message);
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            cc.Close();
+        }
+    }
+}
